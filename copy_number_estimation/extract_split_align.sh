0419dab07d68d94d6c22b2ef75134162e449d889
HYLuu <fobiolu419@gmail.com>
2023-06-12 12:42:46 -0700

    process alignment and call variants

--- copy_number_estimation/extract_split_align.sh
+++ copy_number_estimation/extract_split_align.sh
@@ -4,6 +4,7 @@ set -e
 set -u
 set -o pipefail
 
+# Extract and index reads mapped to NOTCH2 and NOTCH2NL regions (based on GRCh38 on UCSC genome browser) from the CRAM files derived from 1000GP data portal (https://www.internationalgenome.org/data-portal/data-collection/30x-grch38).
 for i in *.final.cram
 	do
 	base=$(basename ${i} .final.cram)
@@ -20,11 +21,14 @@ for i in *.final.cram
 	samtools index ${base}_R.bam
 	done
 
+# For each BAM file gotten from last step, split them into several BAM files based on read groups. This step is needed for marking duplication in GATK.
 for j in *.bam
 	do
 	base=$(basename ${j} .bam)
+	# Get read group ID for each BAM
 	samtools view -H ${j} | grep '^@RG' | awk '{print substr($2,4), substr($9,4), substr($7,4), substr($3,4), substr($5,4)}' > ${base}.RGID
 	IN_FILE="./${base}.RGID"
+	# Split based on read group
 	while read -ra LINE
 		do
 		a="${LINE[0]}"
@@ -32,13 +36,17 @@ for j in *.bam
 		done < "$IN_FILE"
 	done
 
+# Convert the split BAM files into FASTQ files for realignment.
 for k in *_spt.bam
 	do
 	base=$(basename ${k} _spt.bam)
+	# Sorting
 	samtools sort -n -o ${base}.qsort.bam ${k}
+	# BAM to FASTQ
 	bedtools bamtofastq -i ${base}.qsort.bam -fq ${base}_1.fq -fq2 ${base}_2.fq
 	done
 
+# Map FASTQ to reference GRCh38 NOTCH2 and provide read group information. This information is key for downstream GATK functionality. GATK will not work without a read group tag.
 for m in *.qsort.bam
 	do
 	base=$(basename ${m} .qsort.bam)
@@ -46,6 +54,6 @@ for m in *.qsort.bam
 	RG=$(echo $base | cut -d_ -f1-2)
 	line=$(awk '$1 ~ /^${ID}$/' ${RG}.RGID)
 	read id pu sm pl lb <<< ${ID}
-	~/bwa-mem2-2.2.1_x64-linux/bwa-mem2 mem -K 100000000 -Y -R '@RG\tID:${id}\tPU:${pu}\tSM:${sm}\tPL:${pl}\tLB:${lb}' ../../../NOTCH2_hg38_gene.fasta ${base}_1.fq ${base}_2.fq > ${base}_hg38_NOTCH2.sam
+	~/bwa-mem2-2.2.1_x64-linux/bwa-mem2 mem -K 100000000 -Y -R '@RG\tID:${id}\tPU:${pu}\tSM:${sm}\tPL:${pl}\tLB:${lb}' NOTCH2_hg38_gene.fasta ${base}_1.fq ${base}_2.fq > ${base}_hg38_NOTCH2.sam
 	done
 	
--- copy_number_estimation/merge_call_variants.sh
+++ copy_number_estimation/merge_call_variants.sh
@@ -4,74 +4,90 @@ set -e
 set -u
 set -o pipefail
 
-#for i in *.sam
-#	do
-#	base=$(basename ${i} .sam)
-#	samtools view -S -b ${i} > ${base}.bam
-#	samtools sort ${base}.bam -o ${base}.sorted.bam
-#	done
-
-#for j in *.sorted.bam
-#	do
-#	base=$(basename ${j} .sorted.bam)
-#	prefix=$(echo $base | cut -d_ -f1)
-#	samtools merge -c -f ${prefix}_all.bam ${prefix}_*.sorted.bam
-#	done
-
-#mkdir sep_bam
-#mv *_hg38_NOTCH2.sam *_hg38_NOTCH2.sorted.bam *_hg38_NOTCH2.bam sep_bam/
-
-#for k in *_all.bam
-#	do
-#	base=$(basename ${k} _all.bam)
-#	~/gatk-4.4.0.0/gatk MarkDuplicatesSpark -I $k -M ${base}_dedup_metrics.txt -O ${base}_sorted_dedup.bam
-#	done
-
-#for m in *_sorted_dedup.bam
-#	do
-#	base=$(basename ${m} _sorted_dedup.bam)
-#	java -jar ~/picard.jar CollectAlignmentSummaryMetrics R=../../../../NOTCH2_hg38_gene.fasta I=$m O=${base}_aln_metrics.txt
-#	samtools depth -a $m > ${base}_depth_out.txt
-#	~/gatk-4.4.0.0/gatk HaplotypeCaller -R ../../../../NOTCH2_hg38_gene.fasta -I $m -O ${base}_raw_variants.vcf
-#	done
-
-#for n in *_raw_variants.vcf
-#	do
-#	base=$(basename ${n} _raw_variants.vcf)
-#	~/gatk-4.4.0.0/gatk SelectVariants -R ../../../../NOTCH2_hg38_gene.fasta -V $n -select-type SNP -O ${base}_raw_snps.vcf
-#	~/gatk-4.4.0.0/gatk SelectVariants -R ../../../../NOTCH2_hg38_gene.fasta -V $n -select-type INDEL -O ${base}_raw_indels.vcf
-#	~/gatk-4.4.0.0/gatk VariantFiltration -R ../../../../NOTCH2_hg38_gene.fasta -V ${base}_raw_snps.vcf -O ${base}_filtered_snps.vcf \
-#	-filter-name "QD_filter" -filter "QD < 2.0" \
-#	-filter-name "FS_filter" -filter "FS > 60.0" \
-#	-filter-name "MQ_filter" -filter "MQ < 40.0" \
-#	-filter-name "SOR_filter" -filter "SOR > 4.0" \
-#	-filter-name "MQRankSum_filter" -filter "MQRankSum < -12.5" \
-#	-filter-name "ReadPosRankSum_filter" -filter "ReadPosRankSum < -8.0"
-#	~/gatk-4.4.0.0/gatk VariantFiltration -R ../../../../NOTCH2_hg38_gene.fasta -V ${base}_raw_indels.vcf -O ${base}_filtered_indels.vcf \
-#	-filter-name "QD_filter" -filter "QD < 2.0" \
-#	-filter-name "FS_filter" -filter "FS > 200.0" \
-#	-filter-name "SOR_filter" -filter "SOR > 10.0"
-#	~/gatk-4.4.0.0/gatk SelectVariants --exclude-filtered true -V ${base}_filtered_snps.vcf -O ${base}_bqsr_snps.vcf
-#	~/gatk-4.4.0.0/gatk SelectVariants --exclude-filtered true -V ${base}_filtered_indels.vcf -O ${base}_bqsr_indels.vcf
-#	done
-
-for p in *_sorted_dedup.bam
+# Convert the previously aligned (to GRCh38 NOTCH2) SAM to BAM and sort.
+for i in *.sam
 	do
-	base=$(basename ${p} _sorted_dedup.bam)
-	~/gatk-4.4.0.0/gatk BaseRecalibrator -R ../../../../NOTCH2_hg38_gene.fasta -I $p --known-sites ${base}_bqsr_snps.vcf --known-sites ${base}_bqsr_indels.vcf -O ${base}_recal_data.table
-	~/gatk-4.4.0.0/gatk ApplyBQSR -R ../../../../NOTCH2_hg38_gene.fasta -I $p -bqsr ${base}_recal_data.table -O ${base}_recal_reads.bam
-	~/gatk-4.4.0.0/gatk BaseRecalibrator -R ../../../../NOTCH2_hg38_gene.fasta -I ${base}_recal_reads.bam --known-sites ${base}_bqsr_snps.vcf --known-sites ${base}_bqsr_indels.vcf -O ${base}_post_recal_data.table
-	~/gatk-4.4.0.0/gatk HaplotypeCaller -R ../../../../NOTCH2_hg38_gene.fasta -I ${base}_recal_reads.bam -O ${base}_raw_variants_recal.vcf
-	~/gatk-4.4.0.0/gatk SelectVariants -R ../../../../NOTCH2_hg38_gene.fasta -V ${base}_raw_variants_recal.vcf -select-type SNP -O ${base}_raw_snps_recal.vcf
-	~/gatk-4.4.0.0/gatk SelectVariants -R ../../../../NOTCH2_hg38_gene.fasta -V ${base}_raw_variants_recal.vcf -select-type INDEL -O ${base}_raw_indels_recal.vcf
-	~/gatk-4.4.0.0/gatk VariantFiltration -R ../../../../NOTCH2_hg38_gene.fasta -V ${base}_raw_snps_recal.vcf -O ${base}_filtered_snps_final.vcf \
+	base=$(basename ${i} .sam)
+	samtools view -S -b ${i} > ${base}.bam
+	samtools sort ${base}.bam -o ${base}.sorted.bam
+	done
+
+# Merge the sorted BAM from the same individual together.
+for j in *.sorted.bam
+	do
+	base=$(basename ${j} .sorted.bam)
+	prefix=$(echo $base | cut -d_ -f1)
+	samtools merge -c -f ${prefix}_all.bam ${prefix}_*.sorted.bam
+	done
+
+# Clean the directory and keep only the merged BAM
+mkdir sep_bam
+mv *_hg38_NOTCH2.sam *_hg38_NOTCH2.sorted.bam *_hg38_NOTCH2.bam sep_bam/
+
+# Mark duplicates
+for k in *_all.bam
+	do
+	base=$(basename ${k} _all.bam)
+	~/gatk-4.4.0.0/gatk MarkDuplicatesSpark -I $k -M ${base}_dedup_metrics.txt -O ${base}_sorted_dedup.bam
+	done
+
+# Collect Alignment, insert Size Metrics and call variants
+for m in *_sorted_dedup.bam
+	do
+	base=$(basename ${m} _sorted_dedup.bam)
+	# Collect Alignment, insert Size Metrics
+	java -jar ~/picard.jar CollectAlignmentSummaryMetrics R=NOTCH2_hg38_gene.fasta I=$m O=${base}_aln_metrics.txt
+	samtools depth -a $m > ${base}_depth_out.txt
+	# Call variants
+	~/gatk-4.4.0.0/gatk HaplotypeCaller -R NOTCH2_hg38_gene.fasta -I $m -O ${base}_raw_variants.vcf
+	done
+
+# Extract and filter SNPs and Indels
+for n in *_raw_variants.vcf
+	do
+	base=$(basename ${n} _raw_variants.vcf)
+	# Extract from <prefix>_raw_variants.vcf
+	~/gatk-4.4.0.0/gatk SelectVariants -R NOTCH2_hg38_gene.fasta -V $n -select-type SNP -O ${base}_raw_snps.vcf
+	~/gatk-4.4.0.0/gatk SelectVariants -R NOTCH2_hg38_gene.fasta -V $n -select-type INDEL -O ${base}_raw_indels.vcf
+	# Filter 
+	~/gatk-4.4.0.0/gatk VariantFiltration -R NOTCH2_hg38_gene.fasta -V ${base}_raw_snps.vcf -O ${base}_filtered_snps.vcf \
 	-filter-name "QD_filter" -filter "QD < 2.0" \
 	-filter-name "FS_filter" -filter "FS > 60.0" \
 	-filter-name "MQ_filter" -filter "MQ < 40.0" \
 	-filter-name "SOR_filter" -filter "SOR > 4.0" \
 	-filter-name "MQRankSum_filter" -filter "MQRankSum < -12.5" \
 	-filter-name "ReadPosRankSum_filter" -filter "ReadPosRankSum < -8.0"
-	~/gatk-4.4.0.0/gatk VariantFiltration -R ../../../../NOTCH2_hg38_gene.fasta -V ${base}_raw_indels_recal.vcf -O ${base}_filtered_indels_final.vcf \
+	~/gatk-4.4.0.0/gatk VariantFiltration -R NOTCH2_hg38_gene.fasta -V ${base}_raw_indels.vcf -O ${base}_filtered_indels.vcf \
+	-filter-name "QD_filter" -filter "QD < 2.0" \
+	-filter-name "FS_filter" -filter "FS > 200.0" \
+	-filter-name "SOR_filter" -filter "SOR > 10.0"
+	# Exclude filtered variants: extract only the passing variants and provide this as input to BQSR 
+	~/gatk-4.4.0.0/gatk SelectVariants --exclude-filtered true -V ${base}_filtered_snps.vcf -O ${base}_bqsr_snps.vcf
+	~/gatk-4.4.0.0/gatk SelectVariants --exclude-filtered true -V ${base}_filtered_indels.vcf -O ${base}_bqsr_indels.vcf
+	done
+
+# Base quality score recalibration and second round of variant calling
+for p in *_sorted_dedup.bam
+	do
+	base=$(basename ${p} _sorted_dedup.bam)
+	# BQSR
+	~/gatk-4.4.0.0/gatk BaseRecalibrator -R NOTCH2_hg38_gene.fasta -I $p --known-sites ${base}_bqsr_snps.vcf --known-sites ${base}_bqsr_indels.vcf -O ${base}_recal_data.table
+	# Applies the recalibration computed in the first BQSR step to the bam file. This recalibrated bam file is now analysis-ready. 
+	~/gatk-4.4.0.0/gatk ApplyBQSR -R NOTCH2_hg38_gene.fasta -I $p -bqsr ${base}_recal_data.table -O ${base}_recal_reads.bam
+	# For this round of BQSR, recalibrated reads obtained from the Apply BQSR step above are provided as input. 
+	~/gatk-4.4.0.0/gatk BaseRecalibrator -R NOTCH2_hg38_gene.fasta -I ${base}_recal_reads.bam --known-sites ${base}_bqsr_snps.vcf --known-sites ${base}_bqsr_indels.vcf -O ${base}_post_recal_data.table
+	# Second round of variant calling performed using recalibrated (analysis-ready) BAM
+	~/gatk-4.4.0.0/gatk HaplotypeCaller -R NOTCH2_hg38_gene.fasta -I ${base}_recal_reads.bam -O ${base}_raw_variants_recal.vcf
+	~/gatk-4.4.0.0/gatk SelectVariants -R NOTCH2_hg38_gene.fasta -V ${base}_raw_variants_recal.vcf -select-type SNP -O ${base}_raw_snps_recal.vcf
+	~/gatk-4.4.0.0/gatk SelectVariants -R NOTCH2_hg38_gene.fasta -V ${base}_raw_variants_recal.vcf -select-type INDEL -O ${base}_raw_indels_recal.vcf
+	~/gatk-4.4.0.0/gatk VariantFiltration -R NOTCH2_hg38_gene.fasta -V ${base}_raw_snps_recal.vcf -O ${base}_filtered_snps_final.vcf \
+	-filter-name "QD_filter" -filter "QD < 2.0" \
+	-filter-name "FS_filter" -filter "FS > 60.0" \
+	-filter-name "MQ_filter" -filter "MQ < 40.0" \
+	-filter-name "SOR_filter" -filter "SOR > 4.0" \
+	-filter-name "MQRankSum_filter" -filter "MQRankSum < -12.5" \
+	-filter-name "ReadPosRankSum_filter" -filter "ReadPosRankSum < -8.0"
+	~/gatk-4.4.0.0/gatk VariantFiltration -R NOTCH2_hg38_gene.fasta -V ${base}_raw_indels_recal.vcf -O ${base}_filtered_indels_final.vcf \
 	-filter-name "QD_filter" -filter "QD < 2.0" \
 	-filter-name "FS_filter" -filter "FS > 200.0" \
 	-filter-name "SOR_filter" -filter "SOR > 10.0"
