6827158e0b1f55ae2fe080df448225bd84810835
HYLuu <fobiolu419@gmail.com>
2023-06-12 12:01:12 -0700

    add comments

--- SUN_identification/nucdiff_HPRC.sh
+++ SUN_identification/nucdiff_HPRC.sh
@@ -7,6 +7,7 @@ set -o pipefail
 for i in *.fasta
 	do
 	base=$(basename ${i} .fasta)
-	nucdiff --vcf yes ../NOTCH2_hg38_gene ${i} nuc_var ${i}_hg38_NOTCH2
+	# output will be in the directory 'nuc_var/results'
+	nucdiff --vcf yes NOTCH2_hg38_gene ${i} nuc_var ${i}_hg38_NOTCH2
 	done
 
